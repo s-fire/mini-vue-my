f7726245858aa691db8aa0755faa9f6d83ef93a0
gly <274473435@qq.com>
2022-07-13 00:38:22 +0800

    实现mini-vue-ts的effect返回runner和scheduler

--- mini-vue-ts/src/reactive/effect.ts
+++ mini-vue-ts/src/reactive/effect.ts
@@ -1,11 +1,11 @@
 class ReactiveEffect {
   private _fn
-  constructor(fn){
+  constructor(fn, public scheduler?){
     this._fn = fn
   }
   run(){
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 const targetsMap = new Map()
@@ -33,12 +33,18 @@ export function trigger(target,key) {
   let deps = depsMap.get(key)
   if (deps) {
     deps.forEach(effect => {
-      effect.run()
+      if (effect.scheduler) {
+        effect.scheduler()
+      }else{
+        effect.run()
+      }
     });
   }
 }
 let activeEffect
-export function effect(fn){
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn,options:any={}){
+  const scheduler = options.scheduler
+  const _effect = new ReactiveEffect(fn,scheduler)
   _effect.run()
+  return _effect.run.bind(_effect)
 }
--- mini-vue-ts/src/reactive/tests/effect.spec.ts
+++ mini-vue-ts/src/reactive/tests/effect.spec.ts
@@ -14,4 +14,36 @@ describe('effect', () => {
     user.age++
     expect(nextAge).toBe(12)
   });
+  it('should return runner', () => {
+    let foo = 10
+    const runner =  effect(()=>{
+      foo++
+      return 'foo'
+    })
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
+  });
+  it('scheduler', () => {
+    let dummy;
+    let run:any
+    const scheduler = jest.fn(()=>{
+      run = runner
+    })
+    const obj = reactive({foo:1})
+    const runner = effect(()=>{
+      dummy = obj.foo
+    },{scheduler})
+    // scheduler不会被调用
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    obj.foo++
+    // schedule函数被调用一次
+    expect(scheduler).toBeCalledTimes(1)
+    expect(dummy).toBe(1)
+    // 执行effect返回的方法
+    run()
+    expect(dummy).toBe(2)
+  });
 });
